2d2155f6df18a596efc7d9e371709eec86788061
chingeth <145490255+chingeth@users.noreply.github.com>
2023-09-26 15:45:30 -0400

    Add files via upload

---
+++ In-Class Exercise #3.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers = range(1,49)
+# printing 6 random integers
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[2]:
+
+
+#importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers = range(1,49)
+# printing 6 random integers
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[3]:
+
+
+#importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers = range(1,49)
+# printing 6 random integers
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[4]:
+
+
+#importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers = range(1,49)
+# printing 6 random integers
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[5]:
+
+
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[ ]:
+
+
+
+
